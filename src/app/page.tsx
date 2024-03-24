a1a87b1f08e6a7f7dab814c0606c4d025639e1f3
Mahmoud khairy <mahmoudkhairy402@gmail.com>
2024-03-24 23:33:18 +0200

    handle authentication using clerck/next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 // import Image from "next/image";
 
+import Link from "next/link";
 import Dashboard from "./dashboard/page"
-
+import styles from "./page.module.css"
+import { BiSolidLogInCircle } from "react-icons/bi";
 
 export default function Home() {
+  
   return (
-    <div>
-      <Dashboard/>
-    </div>
+    <>
+     <div className={styles.home}>
+     <div className={styles.content}>
+      <h2>welcome to my dashboard</h2>
+      <div className={styles.btns}>
+        <Link href="/sign-in" className={`${styles.login} submit`}>Login now <span className={styles.loginIcon}><BiSolidLogInCircle size={25}/></span></Link>
+        <Link href="/dashboard" className={`${styles.continue} submit`}>continue without login</Link>
+      </div>
+     </div>
+      </div> 
+    </>
   );
 }
--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 function signUp() {
   return (
-    <>
+    <div className="w-100 d-flex justify-content-center align-items-center" style={{height:"100vh"}}>
       <SignUp />
-    </>
+    </div>
   );
 }
 
